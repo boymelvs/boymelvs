6caef274cf5c77e607cf08506c1ae99bc0cbfee5
Melvin <melvin.enmocino@gmail.com>
2024-02-10 16:09:43 +0800

    update cv and upload new certificate

--- src/components/component/Awards.js
+++ src/components/component/Awards.js
@@ -1,3 +1,9 @@
+
+import kodego_cert from "../../images/cv/certificate/kodego_certificate.pdf";
+import bayan_acad_cert from "../../images/cv/certificate/Bayan_Academy_Certificate.pdf";
+
+
+
 const Awards = [
    {
       id: 1,
@@ -9,7 +15,7 @@ const Awards = [
    {
       id: 2,
       course: "Full Stack Web Developer",
-      certificateLink: "",
+      certificateLink: kodego_cert,
       school: "KodegoPH",
       schoolLink: "https://kodego.ph/",
    },
@@ -20,6 +26,22 @@ const Awards = [
       certificateLink: "https://www.coursera.org/account/accomplishments/certificate/W2WE3QEAWYV2",
       school: "Coursera",
       schoolLink: "https://www.coursera.org",
+   },
+
+   {
+      id: 4,
+      course: "Ask Questions to Make Data-Driven Decisions",
+      certificateLink: "https://www.coursera.org/account/accomplishments/certificate/RKE8YMQKVGE3",
+      school: "Coursera",
+      schoolLink: "https://www.coursera.org",
+   },
+
+   {
+      id: 5,
+      course: "Javascript Programming",
+      certificateLink: bayan_acad_cert,
+      school: "Bayan Academy",
+      schoolLink: "",
    },
 ];
 
