12cd0310a95aaa4a855d0b5e1fd0729eed9098e7
Melvin <melvin.enmocino@gmail.com>
2025-12-25 18:17:21 +0800

    updates

--- src/headless/RouteManager.js
+++ src/headless/RouteManager.js
@@ -29,9 +29,9 @@ const RouteManager = (props, context) => {
      }
 
      function handleScroll() {
-          const home = document.getElementById("home");
-          const projects = document.getElementById("projects");
-          const contact = document.getElementById("contact");
+          const home = document.getElementById("home") || "";
+          const projects = document.getElementById("projects") || "";
+          const contact = document.getElementById("contact") || "";
 
           if (window.scrollY >= home.offsetTop - home.clientHeight / 4) {
                setState("activeNavLink", "home");
